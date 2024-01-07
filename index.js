e5738d780b07a35ca143f75f900899d30b5deb16
ashokjr99 <ashoksharmajr@gmail.com>
2024-01-07 05:36:41 -0600

    updatemore

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ let classroomInventory = {
     locked: true,
     interact() {
       // check that the player has a key before unlocking
-      if ("classroom_key" in playerInventory) {
+      if ("hallway_key" in playerInventory) {
         this.locked = false;
         console.log("Your key works! The door is unlocked.");
       } else {
@@ -36,21 +36,16 @@ let classroomInventory = {
       console.log(`${this.description}`);
     },
   },
-  classroom_key: {
-    name: "Classroom Key",
-    description: "A key for the classroom door",
-    take: true,
-    key: "classroom_key",
+
+  hallway_key: {
+    name: "Hallway Key",
+    description: "A key I can use to get into the hallway.",
+    key: "hallway_key",
     look() {
       console.log(`${this.description}`);
     },
-    // take(item) {
-    //   if (item.take === true) {
-    //     this[item.key] = item;
-    //     console.log(`You have taken the ${item}.`);
-    //   }
-    // },
   },
+
   windows: {
     name: "Classroom Windows",
     description: "bullet proof and sealed shut. No chance these will open.",
@@ -62,92 +57,228 @@ let classroomInventory = {
       console.log(`${this.descriptionplus}`);
     },
   },
+
   desk: {
-    name: "Teacher's Desk",
+    name: "A teacher's desk",
     description:
       "It looks like an instructor's desk. There is a key on top of it.",
+    look() {
+      console.log(`${this.name}`);
+    },
     interact() {
       console.log(`${this.description}`);
-      playerInventory.classroom_key;
-      console.log("You have added the key to your inventory");
+      playerInventory["hallway_key"] = {
+        name: "Hallway Key",
+        description: "A key I can use to get into the hallway.",
+        key: "hallway_key",
+        look() {
+          console.log(`${this.description}`);
+        },
+      };
+      delete classroomInventory.hallway_key;
+      console.log("You have added the hallway key to your inventory.");
     },
   },
+
   description:
-    "There is a door here. There is a handwritten sign on the floor. A teacher's desk as well. There are also a number of windows.",
+    "There is a door here, a teacher's desk, and a number of windows.",
 };
+
+// -------------------------------------------------------------
 
 let hallwayInventory = {
   // hallway inventory
+
   door: {
-    name: "Classroom door",
-    description: "The classroom door",
+    name: "Library door",
+    description:
+      "This is the entrace door into the library. Of course, it's locked.",
     locked: true,
-    unlock() {
+    interact() {
       // check that the player has a key before unlocking
-      this.locked = false;
+      if ("library_key" in playerInventory) {
+        this.locked = false;
+        console.log("Your key works! The door is unlocked.");
+      } else {
+        console.log("You do not have a key!");
+      }
     },
-  },
-  hammer: {
-    name: "hammer",
-    description: "Looks sturdy and strong enough to break something",
     look() {
-      console.log(`${this.name}`);
-      return this.description;
-    },
-    swing() {
-      return `You swing the ${this.name}`;
+      console.log(`${this.description}`);
     },
   },
+
+  handwritten_sign: {
+    name: "Handwritten Sign",
+    description:
+      "A handwritten sign. It has six digits on there... 96024? I better jot this down, incase I need it later.",
+    look() {
+      console.log(`${this.description}`);
+    },
+  },
+
+  // hammer: {
+  //   name: "hammer",
+  //   description: "Looks sturdy and strong enough to break something",
+  //   look() {
+  //     console.log(`${this.name}`);
+  //     return this.description;
+  //   },
+  //   swing() {
+  //     return `You swing the ${this.name}`;
+  //   },
+  // },
+
+  safe: {
+    name: "Safe",
+    description:
+      "A metal and locked safe. It seems that there is a passcode that I need to ENTER here...",
+    look() {
+      console.log(`${this.description}`);
+    },
+    interact() {
+      console.log(
+        "Nice! It opened up. Inside the safe are the library keys that I can use to move forward."
+      );
+      console.log("Library keys have now been added to your inventory.");
+      playerInventory["library_key"] = {
+        name: "Library key",
+        description: "A key to get into the library",
+        key: "library_key",
+        look() {
+          console.log(`${this.description}`);
+        },
+      };
+      delete hallwayInventory.library_key;
+    },
+  },
+
   library_key: {
-    name: "Library Key",
+    name: "Library key",
     description: "A key to get into the library",
-    take: true,
     key: "library_key",
     look() {
-      console.log(`${this.name}`);
-      return this.description;
+      console.log(`${this.description}`);
     },
   },
+
+  description:
+    "There is a door here that leads into the library, a safe that is sitting in the corner, and a handwritten sign.",
 };
 
 let libraryInventory = {
   // library inventory
-  principal_key: {
-    name: "Principal's Key",
-    description: "A key to get into any door in the school",
-    look() {
-      console.log(`${this.name}`);
-      return this.description;
-    },
-    take(item) {
-      if (item.take === true) {
-        this[item.key] = item;
-        console.log(`You have taken the ${item}.`);
+  door: {
+    name: "Hallway door",
+    description:
+      "The entrace door into another hallway. It is locked. Maybe my old one works.",
+    locked: true,
+    interact() {
+      // check that the player has a key before unlocking
+      if ("hallway_key" in playerInventory) {
+        this.locked = false;
+        console.log("Your key works! The door is unlocked.");
       } else {
-        console.log(`You cannot take the ${item}.`);
+        console.log("You do not have a key!");
       }
     },
-  },
-};
-
-let principalInventory = {
-  master_key: {
-    name: "Master Key",
-    description: "A key to get out of the school",
     look() {
-      console.log(`${this.name}`);
-      return this.description;
+      console.log(`${this.description}`);
     },
   },
-  take(item) {
-    if (item.take === true) {
-      this[item.key] = item;
-      console.log(`You have taken the ${item}.`);
-    } else {
-      console.log(`You cannot take the ${item}.`);
-    }
+
+  bookshelves: {
+    name: "Bookshelves",
+    description: "A ton of bookshelves that I can LOOK into.",
+    look() {
+      console.log(`${this.description}`);
+    },
+    interact() {
+      console.log(
+        "After combing through a bunch of books, you find a lanyard on one shelf with a bunch of random room keys. Possibly laid here by a janitor. After sorting all of the keys, you see one specifically for the principal's room."
+      );
+      console.log("Pricipal's keys have now been added to your inventory.");
+      playerInventory["principal_key"] = {
+        name: "Principal's key",
+        description: "A key I can use to get into the principal's office.",
+        key: "principal_key",
+        look() {
+          console.log(`${this.description}`);
+        },
+      };
+      delete libraryInventory.principal_key;
+    },
   },
+
+  chair: {
+    name: "chair",
+    description: "A wooden chair.",
+    look() {
+      console.log(`${this.description}`);
+    },
+    interact() {
+      console.log(
+        "You sit on the chair and slouch immediately. This reminds you why your posture it terrible. You get up and move on."
+      );
+    },
+  },
+
+  principal_key: {
+    name: "Principal's key",
+    description: "A key I can use to get into the principal's office.",
+    key: "principal_key",
+    look() {
+      console.log(`${this.description}`);
+    },
+  },
+
+  description:
+    "In this small library, there seems to be some bookshelves, a single chair, and a door that leads to another section of the hallway.",
 };
+
+let hallwayTwoInventory = {
+  // 2nd hallway inventory
+  door: {
+    name: "Principal's door",
+    description: "The entrace door into the Principal's office. It is locked.",
+    locked: true,
+    interact() {
+      // check that the player has a key before unlocking
+      if ("principal_key" in playerInventory) {
+        this.locked = false;
+        console.log("Your key works! The door is unlocked.");
+      } else {
+        console.log("You do not have a key!");
+      }
+    },
+    look() {
+      console.log(`${this.description}`);
+    },
+  },
+
+  exit_door: {
+    name: "Exit Door",
+    description: "The exit door out of the school.",
+    locked: true,
+    interact() {
+      // check that the player has a key before unlocking
+      if ("exit_key" in playerInventory) {
+        this.locked = false;
+        console.log("Your key works! The door is unlocked.");
+      } else {
+        console.log("You do not have a key!");
+      }
+    },
+    look() {
+      console.log(`${this.description}`);
+    },
+  },
+
+  description:
+    "This hallway is empty and only has two doors. One door is the Principal's door, the other is the exit door. My ticket out!",
+};
+
+let principalInventory = {};
 
 let schoolRooms = {
   classroom: ["hallway"],
@@ -181,6 +312,7 @@ async function start() {
     if (action === "interact" || action === "i") {
       currentroomInv[objectUse].interact();
       console.log(playerInventory);
+      console.log(classroomInventory);
     } else if (action === "look" || action === "l") {
       currentroomInv[objectUse].look();
 
